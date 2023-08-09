eae660f93bea1598c8fc37a6aafc06a3513d8a9d
Marksman1593 <bilalazfar1593@gmail.com>
2023-08-09 19:28:24 +0500

    FAQ all functionaloty done.

--- JavaScript/accordian.js
+++ JavaScript/accordian.js
@@ -16,37 +16,44 @@ top1.forEach(acc => {
 
 function toggleCategory(categoryId) {
     const categories = document.querySelectorAll('.categories');
+    let index = 0;
     categories.forEach(category => {
-      if (category.id === categoryId) {
-        category.classList.add('active');
-        const slide = category.querySelector('.slide1');
-        slide.style.transform = 'translateX(0%)';
-        const cls1 = category.querySelectorAll('.cls-1');
-        cls1.forEach(cls => {
-            cls.style.fill ='#fff';
-        });
-        const cls3 = category.querySelectorAll('.cls-3');
-        cls3.forEach(cls => {
-            cls.style.stroke ='#fff';
-        });
-        const p = category.querySelector('.p');
-        p.style.color = "#fff";
-    } else {
-        category.classList.remove('active');
-        const slide = category.querySelector('.slide1');
-        slide.style.transform = '';
-        const computedStyle = window.getComputedStyle(slide);
-        const cls1 = category.querySelectorAll('.cls-1');
-        cls1.forEach(cls => {
-            cls.style.fill ='';
-        });
-        const cls3 = category.querySelectorAll('.cls-3');
-        cls3.forEach(cls => {
-            cls.style.stroke ='';
-        });
-        const p = category.querySelector('.p');
-        p.style.color = "";
-    }
+        index++;
+        const slider = document.querySelectorAll('.slider');
+        if (category.id === categoryId) {
+            console.log(index);
+            slider[index].classList.remove("begone");
+            category.classList.add('active');
+            const slide = category.querySelector('.slide1');
+            slide.style.transform = 'translateX(0%)';
+            const cls1 = category.querySelectorAll('.cls-1');
+            cls1.forEach(cls => {
+                cls.style.fill ='#fff';
+            });
+            const cls3 = category.querySelectorAll('.cls-3');
+            cls3.forEach(cls => {
+                cls.style.stroke ='#fff';
+            });
+            const p = category.querySelector('.p');
+            p.style.color = "#fff";
+        } else {
+            console.log(index);
+            slider[index].classList.add("begone");
+            category.classList.remove('active');
+            const slide = category.querySelector('.slide1');
+            slide.style.transform = '';
+            const computedStyle = window.getComputedStyle(slide);
+            const cls1 = category.querySelectorAll('.cls-1');
+            cls1.forEach(cls => {
+                cls.style.fill ='';
+            });
+            const cls3 = category.querySelectorAll('.cls-3');
+            cls3.forEach(cls => {
+                cls.style.stroke ='';
+            });
+            const p = category.querySelector('.p');
+            p.style.color = "";
+        }
 
     });
-  }
+}
