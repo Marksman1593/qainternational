671b647dead573c364e1f20e2d769845d6133828
Marksman1593 <bilalazfar1593@gmail.com>
2023-08-10 13:14:37 +0500

    Slider scroll speed

--- JavaScript/slider.js
+++ JavaScript/slider.js
@@ -21,8 +21,7 @@ slider.addEventListener("mousemove", (e) => {
   if (!isDown) return;
   e.preventDefault();
   const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 3; //scroll-fast
+  const walk = (x - startX) * 1; //scroll-fast
   slider.scrollLeft = scrollLeft - walk;
   console.log(walk);
 });
-    
