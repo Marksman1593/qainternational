67b622fd6b42f0bda44674d72c417e58079e296a
Marksman1593 <bilalazfar1593@gmail.com>
2023-08-16 15:32:53 +0500

    Services page started

--- JavaScript/slider.js
+++ JavaScript/slider.js
@@ -1,4 +1,34 @@
+// const slider = document.querySelector(".horizontal-slider");
+// let isDown = false;
+// let startX;
+// let scrollLeft;
+
+// slider.addEventListener("mousedown", (e) => {
+//   isDown = true;
+//   slider.classList.add("active");
+//   startX = e.pageX - slider.offsetLeft;
+//   scrollLeft = slider.scrollLeft;
+// });
+// slider.addEventListener("mouseleave", () => {
+//   isDown = false;
+//   slider.classList.remove("active");
+// });
+// slider.addEventListener("mouseup", () => {
+//   isDown = false;
+//   slider.classList.remove("active");
+// });
+// slider.addEventListener("mousemove", (e) => {
+//   if (!isDown) return;
+//   e.preventDefault();
+//   const x = e.pageX - slider.offsetLeft;
+//   const walk = (x - startX) * 1; //scroll-fast
+//   slider.scrollLeft = scrollLeft - walk;
+//   console.log(walk);
+// });
+
 const slider = document.querySelector(".horizontal-slider");
+const content = slider.querySelector(".slider-content");
+const sliderWidth = slider.offsetWidth;
 let isDown = false;
 let startX;
 let scrollLeft;
@@ -9,19 +39,27 @@ slider.addEventListener("mousedown", (e) => {
   startX = e.pageX - slider.offsetLeft;
   scrollLeft = slider.scrollLeft;
 });
+
 slider.addEventListener("mouseleave", () => {
   isDown = false;
   slider.classList.remove("active");
 });
+
 slider.addEventListener("mouseup", () => {
   isDown = false;
   slider.classList.remove("active");
 });
+
 slider.addEventListener("mousemove", (e) => {
   if (!isDown) return;
   e.preventDefault();
   const x = e.pageX - slider.offsetLeft;
   const walk = (x - startX) * 1; //scroll-fast
   slider.scrollLeft = scrollLeft - walk;
-  console.log(walk);
+});
+
+slider.addEventListener("scroll", () => {
+  if (slider.scrollLeft + sliderWidth >= content.offsetWidth) {
+    slider.scrollLeft = 0; // Reset scroll position to the beginning
+  }
 });
